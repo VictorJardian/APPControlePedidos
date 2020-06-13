91c36cc03779ba726f5402b56e7dee79c1929721
Victor Jardian Coelho de Paiva <victorjardiam@gmail.com>
2020-06-13 15:28:47 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APPControlePedidos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
